b98d20e7d620f0764e2302e239fe0f099cbfb4ab
princeden <princedenhom@gmail.con>
2025-05-17 13:28:34 -0400

    projection in place

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import numpy as np
 from PIL import Image
 vertices = []
 faces = []
+textures = []
 FOV = 90
 NEAR = 0.1
 FAR = 100
-IMAGE_WITH = 512
+IMAGE_WIDTH = 512
 IMAGE_HEIGHT = 512
-buffer = np.zeros((IMAGE_HEIGHT, IMAGE_WITH, 3), dtype=np.uint8)
-# projection matrix that scales based on the field of view and aspect ratio
+buffer = np.zeros((IMAGE_HEIGHT, IMAGE_WIDTH, 3), dtype=np.uint8)
+
 def create_projection_matrix(fov, aspect_ratio, near, far):
     scale = 1.0 / np.tan(np.radians(fov) / 2)
     projection_matrix = np.zeros((4, 4))
@@ -31,8 +32,9 @@ def load_obj(file_path):
                     vertices.append(vertex)
                 if line.startswith('f '):
                     face = line.strip().split()
+                    textures = [int(i.split('/')[1]) - 1 for i in face[1:]]
                     face = [int(i.split('/')[0]) - 1 for i in face[1:]]
-                    print(face)
+                    
                     faces.append(face)
     except FileNotFoundError:
         print(f"File {file_path} not found.")
@@ -48,22 +50,26 @@ def homogeneous_to_cartesian(vertex):
 
 def camera_to_raster(vertex, world_to_camera, projection):
     vertex_homogeneous = np.array(vertex + [1])
-
+    # convert from world space to camera space
     camera_space_vertex = np.matmul(vertex_homogeneous, world_to_camera)
     camera_space_vertex = homogeneous_to_cartesian(camera_space_vertex)
-
+    # project from camera space to clip space
     projected_vertex = np.matmul(camera_space_vertex, projection)
+    # convert from clip space to normalized device coordinates
     projected_vertex = homogeneous_to_cartesian(projected_vertex)
+    # check if the vertex is in the view frustum
     if (projected_vertex[0] < -1 or projected_vertex[0] > 1 or
         projected_vertex[1] < -1 or projected_vertex[1] > 1 or
         projected_vertex[2] < NEAR or projected_vertex[2] > FAR):
         return np.inf, np.inf
-    x =min(IMAGE_WITH -1, int((projected_vertex[0] + 1) * (IMAGE_WITH) / 2))
+    # convert from normalized device coordinates to raster space
+    x =min(IMAGE_WIDTH -1, int((projected_vertex[0] + 1) * (IMAGE_WIDTH) / 2))
     y = min(IMAGE_HEIGHT - 1, int((1 - projected_vertex[1] ) * (IMAGE_HEIGHT) / 2))
     return x, y
 
-def edge_function(x0, y0, x1, y1):
-    return (x1 - x0) * (y0 - y1) - (x0 - x1) * (y1 - y0)
+def edge_function(a, b,c):
+    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
+    
 
 def main():
     load_obj('cow.obj')
@@ -73,12 +79,37 @@ def main():
     world_to_camera[3, 1] = -10
     world_to_camera[2, 3] = -100
     projection = create_projection_matrix(FOV, 1.0, NEAR, FAR)
-    for face in faces:
-        triangle = [vertices[vertex] for vertex in face]
-        for vertex in triangle:
-            x, y = camera_to_raster(vertex, world_to_camera, projection)
-            if x != np.inf and y != np.inf:
-                buffer[y, x] = [255, 255, 255]
+    depth_buffer = np.full((IMAGE_HEIGHT, IMAGE_WIDTH), np.inf)
+    for i in range(len(faces)):
+        face = faces
+        triangle = [camera_to_raster(vertices[vertex], world_to_camera,projection) for vertex in face]
+        
+        xmax = max(triangle[0][0], triangle[1][0], triangle[2][0], IMAGE_WIDTH - 1)
+        xmin = min(triangle[0][0], triangle[1][0], triangle[2][0], 0)
+        ymax = max(triangle[0][1], triangle[1][1], triangle[2][1], IMAGE_HEIGHT - 1)
+        ymin = min(triangle[0][1], triangle[1][1], triangle[2][1], 0)
+        for y in range(ymin, ymax + 1):
+            for x in range(xmin, xmax + 1):
+        
+                area = edge_function(triangle[0], triangle[1], triangle[2]) 
+                pixel = np.array([x+ 0.5, y+0.5, 0])
+                w0 = edge_function(triangle[0], triangle[1], pixel)
+                w1 = edge_function(triangle[1], triangle[2], pixel)
+                w2 = edge_function(triangle[2], triangle[0], pixel)
+                # check if the pixel is inside the triangle
+                if (w0 >= 0 and w1 >= 0 and w2 >= 0) or (w0 <= 0 and w1 <= 0 and w2 <= 0):
+                    # compute the barycentric coordinates
+                    w0 = w0 / area
+                    w1 = w1 / area  
+                    w2 = w2 / area
+                    z = 1 / (triangle[0][2] * w0 + triangle[1][2] * w1 + triangle[2][2] * w2) 
+                    # texture_vector = textures[i]
+                    # texture_vector = map(lambda n: n * z, texture_vector)
+                    # texture = texture_vector[0] * w0 + texture_vector[1] * w1 + texture_vector[2] * w2
+                    if depth_buffer[y, x] > z:
+                        depth_buffer[y, x] = z
+                        buffer[y, x] = [255, 255, 255]
+                    
     image = Image.fromarray(buffer)
     image.show()
 
